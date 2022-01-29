e9d2fc6f8fd16326b755ee1d4ffe47f4856b9216
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2022-01-29 21:28:12 +0200

    Favicon and icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,23 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://nogelai.net/",
     title: "nogelainet",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass", 
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Nogelainet`,
+        short_name: `Nogelainet`,
+        start_url: `/`,
+        background_color: `#F2EFE9`,
+        theme_color: `#0B0B0B`,
+        display: `standalone`,
+        icon: "src/images/icon.png"
+      },
+    }
+  ],
 };
