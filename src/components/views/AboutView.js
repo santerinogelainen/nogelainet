4d61a3f5524731f3e22004f73b28c70b1eedd484
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2022-02-18 22:47:25 +0200

    About view

--- src/components/views/AboutView.js
+++ src/components/views/AboutView.js
@@ -1,5 +1,4 @@
 import React from "react";
-import WrittenImage from "../effects/WrittenImage";
 import FadeAnimation from "../animations/FadeAnimation";
 import ReactMarkdown from "react-markdown"
 import { useDidMountEffect } from "../../utils/reactUtils";
@@ -10,28 +9,25 @@ const AboutView = ({
     onComplete = null
 }) => {
 
-    const [textVisible, setTextVisible] = React.useState(false);
+    const [visible, setVisible] = React.useState(false);
 
     useDidMountEffect(() => {
 
-        if (!textVisible) {
-            setTextVisible(true);
+        if (!visible) {
+            setVisible(true);
         }
 
-    }, [textVisible]);
+    }, [visible]);
 
     return (
         <div className="about">
             <div className="about-text-container">
-                <FadeAnimation visible={textVisible} speed={400} after={onComplete}>
+                <FadeAnimation visible={visible} speed={400} after={onComplete}>
                     <ReactMarkdown className="about-text">
                         {text}
                     </ReactMarkdown>
                 </FadeAnimation>
             </div>
-            {/* <div className="about-image">
-                <WrittenImage src={image} speed={400} maxSize={35} afterText={() => setTextVisible(true)} />
-            </div> */}
         </div>
     )
 }
