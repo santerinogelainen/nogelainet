8ca49d512a4628fa89e8bcce969ce13c2efba559
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2024-12-02 22:12:28 +0200

    Temp

---
+++ src/components/projects/ProjectImage.stories.tsx
@@ -0,0 +1,16 @@
+import ProjectImage from "./ProjectImage";
+import { Meta, StoryObj } from "@storybook/react/*";
+
+const meta: Meta<typeof ProjectImage> = {
+  title: "Components/ProjectImage",
+  component: ProjectImage,
+};
+
+export default meta;
+type Story = StoryObj<typeof ProjectImage>;
+
+export const Default: Story = {
+  args: {
+    src: "https://picsum.photos/1000",
+  },
+};
