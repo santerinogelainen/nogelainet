6fdb1eb3a56b20a77b859f347e3bcda7c339369b
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2024-12-05 00:18:12 +0200

    YTJ text

--- src/pages/projects/ytj.tsx
+++ src/pages/projects/ytj.tsx
@@ -22,8 +22,49 @@ const Page = () => {
       tags={["React", "JavaScript", "TypeScript", ".NET", "C#", "SQL Server"]}
       onComplete={() => dispatch(viewActions.setControlsVisible(true))}
     >
-      <Language lang="en"></Language>
-      <Language lang="fi"></Language>
+      <Language lang="en">
+        <p>
+          The Business Information System (YTJ) is a trade register for finnish
+          companies managed by the Finnish Patent and Registration Office (PRH).
+          At Knowit I worked for PRH to modernize their online service for a
+          registering a limited liability company.
+        </p>
+        <p>What I developed and worked on:</p>
+        <ul>
+          <li>
+            Front-end and backend development for entering company address
+            information
+          </li>
+          <li>Front-end development for entering shareholders</li>
+          <li>Front-end development for entering accountants</li>
+          <li>Front-end development for entering board of directors</li>
+          <li>Front-end development for entering ceo/vp</li>
+          <li>Front-end development for digital signing</li>
+          <li>Front-end and backend development for payments</li>
+        </ul>
+      </Language>
+      <Language lang="fi">
+        <p>
+          Yritystietojärjestelmä (YTJ) on yritysrekisteri, jota hallinnoi
+          Patentti- ja rekisterihallitus (PRH). Knowitilla työskentelin PRH:lle
+          osakeyhtiön perustamisen uudistusprojektin parissa.
+        </p>
+        <p>Kehitin mm. seuraavia toiminnallisuuksia:</p>
+        <ul>
+          <li>
+            Front-end ja backend toteutus yrityksen osoitetietojen syöttämiselle
+          </li>
+          <li>Front-end toteutus osakkeenomistajien syöttämiselle</li>
+          <li>Front-end toteutus tilintarkastajien syöttämiselle</li>
+          <li>Front-end toteutus hallituksen jäsenten syöttämiselle</li>
+          <li>
+            Front-end toteutus toimitusjohtajan ja varatoimitusjohtajan
+            syöttämiselle
+          </li>
+          <li>Front-end toteutus digitaaliselle allekirjoitukselle</li>
+          <li>Front-end ja backend toteutus maksuprosessille</li>
+        </ul>
+      </Language>
     </ProjectView>
   );
 };
