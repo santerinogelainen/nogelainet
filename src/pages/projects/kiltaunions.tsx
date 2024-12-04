51e465f442955efe7548b44d9a9924b999cf6f51
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2024-12-04 23:43:22 +0200

    Add kilta unions text

--- src/pages/projects/kiltaunions.tsx
+++ src/pages/projects/kiltaunions.tsx
@@ -5,6 +5,8 @@ import { HeadLayout } from "../../layout";
 import ProjectView from "../../components/views/ProjectView";
 import Language from "../../components/Language";
 import { useAppDispatch } from "../../state/store";
+import { Link } from "gatsby";
+import { Pages } from "../../models/pages";
 
 export const Head = () => {
   const { t } = useTranslation();
@@ -30,8 +32,48 @@ const Page = () => {
       ]}
       onComplete={() => dispatch(viewActions.setControlsVisible(true))}
     >
-      <Language lang="en"></Language>
-      <Language lang="fi"></Language>
+      <Language lang="en">
+        <p>
+          Kilta for Unions or "Union Kilta" is a membership registry for finnish
+          Workers Unions, that I developed and maintained at Kehätieto.
+        </p>
+        <p>Features I implemented and worked on:</p>
+        <ul>
+          <li>Advanced selection (reporting tool)</li>
+          <li>
+            Introduction of the{" "}
+            <Link to={Pages.Projects.KTVueComponents}>kt-vuecomponents</Link>{" "}
+            component library to the code base
+          </li>
+          <li>Search for pages in quick search</li>
+          <li>Search view for employers and workplaces in the registry</li>
+          <li>Improved translation and localization functionality</li>
+          <li>Static reports for customers</li>
+          <li>Other user experience improvements</li>
+          <li>Accessability improvements</li>
+        </ul>
+      </Language>
+      <Language lang="fi">
+        <p>
+          Kilta Ammattiliitoille eli "Ammattiliitto Kilta" on suomalaisille
+          ammattiliitoille tarkoitettu jäsenrekisteri, jota kehitin ja ylläpidin
+          Kehätiedolla.
+        </p>
+        <p>Ominaisuudet, joita toteutin ja ylläpidin:</p>
+        <ul>
+          <li>Edistynyt poiminta (raportointityökalu)</li>
+          <li>
+            <Link to={Pages.Projects.KTVueComponents}>kt-vuecomponents</Link>{" "}
+            komponenttikirjaston lisääminen koodikannan osaksi
+          </li>
+          <li>Sivujen haku pikahaku-toiminnolla</li>
+          <li>Työnantajien ja työpaikkojen hakunäkymä rekisterissä</li>
+          <li>Parannettu käännös- ja lokalisointitoiminnallisuus</li>
+          <li>Staattiset raportit asiakkaille</li>
+          <li>Muita käyttäjäkokemuksen parannuksia</li>
+          <li>Saavutettavuusparannuksia</li>
+        </ul>
+      </Language>
     </ProjectView>
   );
 };
